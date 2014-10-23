1e5c16a04aa8cc4b5ee35f75ebe71314e9101f1a
simonorlovsky <simon.orlovsky@yahoo.com>
2014-10-23 10:11:35 -0500

    Edited README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+Concepts to be mastered in this applcation:
+1. Using PHP and HTML to create static webpages.
+2. Using PHP, MySQL and HTML to create dynamic webpages.
+3. Become familiar with SQL queries.
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+$cars=array("Volvo","BMW","Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+?>
+
+
+</body>
+</html>
